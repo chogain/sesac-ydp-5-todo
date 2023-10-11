2371081612009305d2ecfeb0a95d3e76e420630e
Cho Gain <chogain05@gmail.com>
2023-10-11 18:20:32 +0900

    feat: todo 리스트 보이기

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Todo from './components/Todo';
+import './styles/App.css';
 
 function App() {
+  const [todoItems, setTodoItems] = useState([
+    {
+      id: 1,
+      title: 'my todo1',
+      done: false,
+    },
+    {
+      id: 2,
+      title: 'my todo2',
+      done: false,
+    },
+    {
+      id: 3,
+      title: 'my todo3',
+      done: true,
+    },
+    {
+      id: 4,
+      title: '토이프로젝트 완성',
+      done: false,
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {todoItems.map((item) => (
+        <Todo key={item.id} item={item} />
+      ))}
     </div>
   );
 }
