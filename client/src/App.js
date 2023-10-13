80d8985b041f38c34f67d59e703f66fec778a27c
Cho Gain <chogain05@gmail.com>
2023-10-13 17:32:57 +0900

    study: crud, api 연결

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,91 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Todo from './components/Todo';
 import './styles/App.css';
 import AddTodo from './components/AddTodo';
+import axios from 'axios';
+
 function App() {
-  const [todoItems, setTodoItems] = useState([
-    {
-      id: 1,
-      title: 'my todo1',
-      done: false,
-    },
-    {
-      id: 2,
-      title: 'my todo2',
-      done: false,
-    },
-    {
-      id: 3,
-      title: 'my todo3',
-      done: true,
-    },
-    {
-      id: 4,
-      title: '토이프로젝트 완성',
-      done: false,
-    },
-  ]);
+  // console.log(process.env.REACT_APP_DB_HOST);
+  // const [todoItems, setTodoItems] = useState([
+  //   {
+  //     id: 1,
+  //     title: 'my todo1',
+  //     done: false,
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'my todo2',
+  //     done: false,
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'my todo3',
+  //     done: true,
+  //   },
+  //   {
+  //     id: 4,
+  //     title: '토이프로젝트 완성',
+  //     done: false,
+  //   },
+  // ]);
+
+  const [todoItems, setTodoItems] = useState([]);
+  useEffect(() => {
+    const getTodos = async () => {
+      const res = await axios.get(`${process.env.REACT_APP_DB_HOST}/todos`);
+      setTodoItems(res.data);
+    };
+
+    getTodos();
+  }, []);
 
   // todoItems 상태에 새로운 todo를 추가하는 일
-  const addItem = (newItem) => {
-    console.log(newItem); // { title: '저녁먹기' }
+  const addItem = async (newItem) => {
+    // console.log(newItem); // { title: '저녁먹기' }
 
-    // newItem id 키 값, done 키 값 넣기
-    newItem.id = todoItems.length + 1;
-    const { id, title, done } = newItem;
-    // 투두 리스트에 새로운 투두를 추가
-    setTodoItems([
-      ...todoItems,
-      {
-        id,
-        title,
-        done,
-      },
-    ]);
+    // // newItem id 키 값, done 키 값 넣기
+    // newItem.id = todoItems.length + 1;
+    // newItem.done = false;
+
+    // // todoItems 배열에 새로운 todoItem 추가
+    // setTodoItems([...todoItems, newItem]);
+
+    const res = await axios.post(`${process.env.REACT_APP_DB_HOST}/todo}`, {
+      newItem,
+    });
+
+    setTodoItems([...todoItems, res.data]);
   };
-  // 투두 리스트에서 투두 삭제
-  const deleteItem = (item) => {
+
+  // todoItems에서 todo 삭제
+  const deleteItem = async (item) => {
+    await axios.delete(`${process.env.REACT_APP_DB_HOST}/todo/${item.id}}`);
+
     const newTodo = todoItems.filter((rowData) => rowData.id !== item.id);
     setTodoItems(newTodo);
   };
+
+  const updateItem = async (targetItem) => {
+    await axios.patch(
+      `${process.env.REACT_APP_DB_HOST}/todo/${item.id}}`,
+      targetItem
+    ); // axios.patch('url', {})
+  };
+
   return (
     <div className='App'>
       <AddTodo addItem={addItem} />
       {/* todoItems 반복, props로 데이터(투두 객체)를 자식 컴포넌트에게 전달 */}
-      {todoItems.map((item) => {
-        return <Todo key={item.id} item={item} deleteItem={deleteItem} />;
+      <div>{todoItems.length} Todos</div>
+      {todoItems.reverse().map((item) => {
+        return (
+          <Todo
+            key={item.id}
+            item={item}
+            deleteItem={deleteItem}
+            updateItem={updateItem}
+          />
+        );
       })}
     </div>
   );
--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,52 +1,57 @@
 import React, { useState } from 'react';
-export default function Todo({ item, deleteItem }) {
+export default function Todo({ item, deleteItem, updateItem }) {
   const [todoItem, setTodoItem] = useState(item);
   const { id, title, done } = todoItem;
   const [readOnly, setReadOnly] = useState(true);
+
   const onDeleteBtnClick = () => {
     deleteItem(todoItem);
   };
+
   // title 클릭시 readonly를 false로 변경
   const offReadOnlyMode = () => {
     //setReadOnly(false);
   };
+
   const editEventHandler = (e) => {
     const { title, ...rest } = todoItem;
     console.log(rest);
-    /*
-    rest
-    {
-      id : ~~,
-      done : ~~,
-    }
-    */
+
     setTodoItem({
       title: e.target.value,
       ...rest,
     });
   };
+
   const editKey = (e) => {
     if (e.key === 'Enter') {
       setReadOnly(true);
     }
   };
+
   // 포커스 인
   const onFocus = (e) => {
     console.log(e);
     setReadOnly(false);
   };
+
   // 포커스 아웃
   const onBlur = (e) => {
     console.log(e);
     setReadOnly(true);
   };
+
   const checkboxEventHandler = (e) => {
     const { done, ...rest } = todoItem;
-    setTodoItem({
+    const updatedItem = {
       done: e.target.checked,
       ...rest,
-    });
+    };
+
+    setTodoItem(updatedItem);
+    updateItem(updatedItem);
   };
+
   return (
     <div>
       <input
